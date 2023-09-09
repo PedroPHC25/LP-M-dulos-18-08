92789876c123ef8cf707f3ad6fde1d9cac30d509
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-09-09 17:21:08 -0300

    first commit

---
+++ module.py
@@ -0,0 +1,14 @@
+"""
+Módulo simples para exemplo
+"""
+
+def saudacao():
+    """Teste"""
+    print("Eu adoro a EMAp!")
+
+var = "O Yuri é um grande homem"
+
+if __name__ == "__main__":
+    saudacao()
+
+print(__name__)
---
+++ numpy-aula-0.py
@@ -0,0 +1,6 @@
+import module
+
+print("Meu programa")
+module.saudacao()
+
+print(dir(module))
